2746bd9c94f70dc56a80cc769213301c96d66080
Prashant1304 <sinha.prashant134@gmail.com>
2021-09-18 20:14:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import {API_KEY} from './componts/config'
 export default function App() {
-  
+    
     const [data, setData] = useState([]);
     const [value, setValue] = useState("");
+    const [bool, setBool] = useState(false);
+
     const [locationData, setLocationData] = useState("");
     const date = new Date()
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const day = date.getDay()
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationData}&appid=${API_KEY}`;
-    
+
     useEffect(() => {
         axios(apiUrl).then((response) => {
         setData(response.data);
@@ -31,6 +33,7 @@ export default function App() {
     
     function handleButton() {
         setLocationData(value )
+        setBool(true)
     }
 
     function kelvinToFarenheit  (k)  {
@@ -44,93 +47,95 @@ export default function App() {
   
   return (
     <div className="App">
+        <div className="img-main">
+        <img className="img" src="https://www.kenya.campusfrance.org/sites/pays/files/kenya/new-history-forecast-bulk_0.png" ></img>
+        </div>
+        <div className = "positioning" >
+            <div classNam = "header" >
 
+                <div className = "sub-header" >
+                    <div>
+                        <p> Please enter city name </p>
+                   </div>
 
-        <div classNam = "header" >
-            
-            <div className = "sub-header" >
-                <div>
-                    <p> Please enter city name </p>
-               </div>
-
-               <div>
-                    <input type="text" onChange={handleInput}  value={value} />
+                   <div>
+                        <input type="text" onChange={handleInput}  value={value} />
+                    </div>
+                    <div>
+                        <button onClick={handleButton} >Search</button>
+                    </div>
                 </div>
-                <div>
-                    <button onClick={handleButton} >Search</button>
-                </div>
+
             </div>
 
-        </div>
+            <div className = "center" >
+                {bool&&
+                <div>
+                    {data.main ? (
+                        <div>
 
-        <div className = "center" >
-            
-            <div>
-                {data.main ? (
-                    <div>
+                                <div className = "C-eather">
+                                    <div>
+                                        <p>Current Weather:</p>
+                                    </div>
 
-                            <div className = "C-eather">
-                                <div>
-                                    <p>Current Weather:</p>
+                                    <div>
+                                        {days[day]}  {date.getDate()}
+                                    </div>
+                                </div>
+                                <div className = "temp" >
+                                    <div>
+                                        <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}></img>
+                                    </div>
+
+                                    <div className = "temp-celcious">
+                                        {kelvinToFarenheit(data.main.temp)}&#176;C
+                                    </div>
                                 </div>
 
-                                <div>
-                                    {days[day]}  {date.getDate()}
-                                </div>
-                            </div>
-                            <div className = "temp" >
-                                <div>
-                                    <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}></img>
+                            <div className = "description" >
+                                <div className = "sub-description">
+
+                                    <div>
+                                        {data.weather[0].description}
+                                    </div>
+
+                                    <div>
+                                        feels like:  {kelvinToFarenheit(data.main.feels_like)}&#176;C
+                                    </div>
+
+                                    <div>
+                                        Humidity:  {data.main.humidity}%
+                                    </div>
                                 </div>
 
-                                <div className = "temp-celcious">
-                                    {kelvinToFarenheit(data.main.temp)}&#176;C
-                                </div>
-                            </div>
-                        
-                        <div className = "description" >
-                            <div className = "sub-description">
+                                <div className = "sub-description">
+                                    <div>
+                                        Wind:    W {data.wind.speed}km/h 
+                                    </div>
 
-                                <div>
-                                    {data.weather[0].description}
-                                </div>
+                                    <div>
+                                        pressure: {data.main.pressure} mbar
+                                    </div>
 
-                                <div>
-                                    feels like:  {kelvinToFarenheit(data.main.feels_like)}&#176;C
-                                </div>
-
-                                <div>
-                                    Humidity:  {data.main.humidity}%
-                                </div>
-                            </div>
-                            
-                            <div className = "sub-description">
-                                <div>
-                                    Wind:    W {data.wind.speed}km/h 
-                                </div>
-
-                                <div>
-                                    pressure: {data.main.pressure} mbar
-                                </div>
-
-                                <div>
-                                    visibility:  {meterToKiloMeter( data.visibility)} km
+                                    <div>
+                                        visibility:  {meterToKiloMeter( data.visibility)} km
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ):(
-                    <div>
-                        Loading...
-                    </div>
-                ) }
-            </div>
+                    ):(
+                        <div>
+                            
+                        </div>
+                    ) }
+                </div>}
 
-        </div>
+            </div>
       <div>
         {/* {data.coord.lat} */}
       </div>
-      
+      </div>
     </div>
   );
 }
